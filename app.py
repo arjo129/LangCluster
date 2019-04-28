8bfc28539a1745dffceea07729fd38b5437b9f9d
Arjo Chakravarty <arjo129@gmail.com>
2019-04-28 15:10:27 +0800

    Added support for auto-dbscan parameter setting

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 from json import load, dumps
 from sklearn.cluster import DBSCAN
 from backends.microsoft_adapter import MSTranslationProvider
+from transliterate import translit
+from fuzzy import DMetaphone
 import cyrtranslit
 import unidecode
 import numpy as np
@@ -52,11 +54,17 @@ def post_process(word, language):
     """
     if language in config.get_languages_by_property("transliterate", "cyr"):
         word = cyrtranslit.to_latin(word, language).casefold()
+    if language in config.get_languages_by_property("transliterate", "trans"):
+        word = translit(word, language, reversed=True).casefold()
     if language in config.get_languages_if_property_exists("accents"):
         word = unidecode.unidecode(word).casefold()
     if language in config.get_languages_if_property_exists("stopwords"):
         res = word.casefold().split()
         res = list(filter(lambda x: x not in config.get_language(language)["stopwords"], res))
+        word = " ".join(res)
+    if language in config.get_languages_if_property_exists("stopwords-arabic"):
+        res = word.casefold().split("-")
+        res = list(filter(lambda x: x not in ["al"], res))
         word = " ".join(res)
     return word
 
@@ -95,6 +103,20 @@ def get_levenshtein_distance(word1, word2):
     return matrix[len(word1)][len(word2)]
 
 
+
+def soundex_and_levenstein(word1, word2):
+    dmeta = DMetaphone()
+    sd1 = dmeta(word1)
+    sd2 = dmeta(word2)
+    dist = float('inf')
+    for i in sd1:
+        for j in sd2:
+            if i != None and j != None:
+                dist_meta = get_levenshtein_distance(i,j)
+                dist = min(dist_meta, dist)
+    dist_lv = get_levenshtein_distance(word1, word2)
+    return min(dist, dist_lv)
+
 def translate_all_backends(word):
     translations = MSBackend.get_translation(word)
     translations["en"] = word
@@ -114,29 +136,41 @@ def generate_edit_matrix(word, distance_metric=get_levenshtein_distance):
         j = 0
         lang_order.append(lang)
         for lang2 in translations:
-            distance_matrix[i, j] = distance_metric(translations[lang], translations[lang2])
+            try:
+                distance_matrix[i, j] = distance_metric(translations[lang], translations[lang2])
+            except UnicodeError:
+                print("Warning langpair "+lang+", "+lang2+"Giving UnicodeError!!")
+                distance_matrix[i, j] = 10000
             j += 1
         i += 1
     return distance_matrix, lang_order, translations
 
 
 def cluster_matrix(distance_matrix):
-    dbscan = DBSCAN(eps=1, min_samples=2, metric="precomputed")
-    return dbscan.fit_predict(distance_matrix)
+    best_score = 0
+    best_res = []
+    for i in range(1, 3):
+        dbscan = DBSCAN(eps=i, min_samples=2, metric="precomputed")
+        res = dbscan.fit_predict(distance_matrix)
+        score = max(res) / len(list(filter(lambda x: x == -1, res)))
+        if  best_score  < score:
+            best_score = score
+            best_res = res
+    return best_res
 
 @app.route('/static')
 def send_js(path):
     return send_from_directory('static', path)
 
 @app.route('/api/backend')
-def home_page():
-    word = "tea"
+def backend():
+    word = request.args["word"]
     edit_matrix, lang_order, translations = generate_edit_matrix(word)
     clusters = cluster_matrix(edit_matrix)
     dct = {}
     for language, cluster in zip(lang_order, clusters):
         for region in config.get_language(language)["regions"]:
-            dct[region] = int(cluster)
+            dct[region] = {"group": int(cluster), "language": config.get_language(language)["name"], "pronunciation": translations[language]}
     return dumps(dct)
 
 
